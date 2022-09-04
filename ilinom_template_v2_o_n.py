5f110eb3c3b836dbe39ed7f0714d56a616518a17
q4x2 <q4x2@users.noreply.github.com>
2022-09-04 12:28:43 -0500

    Update ilinom_template_v2_o_n.py

--- ilinom_template_v2_o_n.py
+++ ilinom_template_v2_o_n.py
@@ -1,11 +1,3 @@
-def is_templ_match(tmpl_dict):
-    print(tmpl_dict)
-    for char in tmpl_dict:
-        if tmpl_dict[char]['cnt'] != tmpl_dict[char]['m_cnt']:
-            return False
-    return True
-
-
 def calc_templ_match(str, tmpl):
     tmpl_dict = {}
     for char in tmpl:
@@ -14,16 +6,28 @@ def calc_templ_match(str, tmpl):
         else:
             tmpl_dict[char]['cnt'] += 1
     epoch = 0
+    tmpl_sz = len(tmpl)
+    gl_cnt = 0
     for char in str:
         if char in tmpl_dict:
             if epoch == tmpl_dict[char]['epoch']:
                 tmpl_dict[char]['m_cnt'] += 1
                 if tmpl_dict[char]['m_cnt'] > tmpl_dict[char]['cnt']:
                     epoch += 1
+                    gl_cnt = 1
                     tmpl_dict[char]['m_cnt'] = 1
+                    tmpl_dict[char]['epoch'] = epoch
+                else:
+                    gl_cnt += 1
+                    if gl_cnt == tmpl_sz:
+                        return True
             else:
                 tmpl_dict[char]['m_cnt'] = 1
                 tmpl_dict[char]['epoch'] = epoch
+                gl_cnt += 1
+                if gl_cnt == tmpl_sz:
+                    return True
         else:
             epoch += 1
-    return is_templ_match(tmpl_dict)
+            gl_cnt = 0
+    return False
